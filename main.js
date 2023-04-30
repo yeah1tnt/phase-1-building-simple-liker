696693a3c9d726b3653327ddf1582f10fa7828fc
Tong Tang <pichip22@hotmail.com>
2023-04-30 17:04:50 -0400

    completed

--- main.js
+++ main.js
@@ -3,10 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const error = document.getElementById('modal');
+error.className = "hidden";
 
+function handleLike(e){
+  const heart = e.target;
+  mimicServerCall()
+  .then( () => {
+    if(heart.innerText === EMPTY_HEART ){
+      heart.innerText = FULL_HEART;
+      heart.className = "activated-heart";
+    }
+    else {
+      heart.innerText = EMPTY_HEART;
+      heart.className = "";
+    }
+  })
+    .catch(error => {
+    const modal = document.querySelector("#modal");
+    modal.className = "";
+    modal.innerText = error;
+    setTimeout(() => modal.className = "hidden", 3000 );
+  })
+  };
 
-
-
+  const likeButtons = Array.from(document.getElementsByClassName('like-glyph'));
+  likeButtons.forEach(likeGlyph => likeGlyph.addEventListener('click', handleLike));
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
